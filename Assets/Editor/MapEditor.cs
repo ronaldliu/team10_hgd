237e043f5829772830e57c148d9709abec543e1a
Jesse Moore <jessem@mtu.edu>
2017-03-20 21:42:07 -0400

    MapEditor Pt.5

--- Assets/Editor/MapEditor.cs
+++ Assets/Editor/MapEditor.cs
@@ -6,10 +6,13 @@ public class MapEditor : EditorWindow {
 
 	bool mapInScene;
 	public static Transform platforms, startLocation, endFlag, outOfBounds, powerUps;
+	List<GameObject> placablePlatforms;
 	List<GameObject> placablePowerUps;
 	List<GameObject> placableWeapons;
 	GameObject mousedOver;
 
+	static bool platSnap;
+	static float platSnapSize;
 	static GameObject draggedObj;
 
 	//Texture saw; //TODO test
@@ -42,10 +45,16 @@ public class MapEditor : EditorWindow {
 			}
 		}
 
-		if (placablePowerUps == null) {
+		if (placablePlatforms == null) {
+			placablePlatforms = new List<GameObject> ();
 			placablePowerUps = new List<GameObject> ();
 			placableWeapons = new List<GameObject> ();
-			Object[] placables = Resources.LoadAll ("PowerUps/");
+			Object[] placables = Resources.LoadAll ("Platforms/");
+			foreach (Object obj in placables) {
+				if (((GameObject)obj).GetComponent<PlatformController> () != null)
+					placablePlatforms.Add ((GameObject)obj);
+			}
+			placables = Resources.LoadAll ("PowerUps/");
 			foreach (Object obj in placables) {
 				if (((GameObject)obj).GetComponent<PickUpPower> () != null)
 					placablePowerUps.Add ((GameObject)obj);
@@ -65,47 +74,79 @@ public class MapEditor : EditorWindow {
 		mapInScene = false;
 		meme = false;
 		mapName = "Map";
+		platSnap = true;
+		platSnapSize = 1.0f;
 	}
 
 	private static void OnSceneGUI(SceneView sceneview)
 	{
-		Handles.BeginGUI();
+		Handles.BeginGUI ();
 
 		//if (GUILayout.Button("Press Me"))
 		//	Debug.Log("Got it to work.");
 
-		if(meme)
-			GUI.Label (new Rect(Screen.width - 250, Screen.height - 250, 250, 250), Resources.Load("meme") as Texture);
+		if (meme)
+			GUI.Label (new Rect (Screen.width - 250, Screen.height - 250, 250, 250), Resources.Load ("meme") as Texture);
 
 
-		if (Event.current.type == EventType.DragUpdated || Event.current.type == EventType.DragPerform)
-		{
+		if (Event.current.type == EventType.DragUpdated || Event.current.type == EventType.DragPerform) {
 			DragAndDrop.visualMode = DragAndDropVisualMode.Copy; // show a drag-add icon on the mouse cursor
 
 			if (draggedObj == null)
-				draggedObj = (GameObject)Object.Instantiate(DragAndDrop.objectReferences[0]);
+				draggedObj = (GameObject)Object.Instantiate (DragAndDrop.objectReferences [0]);
 
 			Vector3 mousePos = Event.current.mousePosition;
 			mousePos.y = sceneview.camera.pixelHeight - mousePos.y;
 			mousePos = sceneview.camera.ScreenToWorldPoint (mousePos);
 			mousePos.z = 0;
+			//Snap platform to grid
+			if (draggedObj.GetComponent<PlatformController> () && platSnap) {
+				mousePos.x = Mathf.Round (mousePos.x / platSnapSize) * platSnapSize;
+				mousePos.y = Mathf.Round (mousePos.y / platSnapSize) * platSnapSize;
+			}
 			draggedObj.transform.position = mousePos;
 
-			if (Event.current.type == EventType.DragPerform)
-			{
-				DragAndDrop.AcceptDrag();
+			if (Event.current.type == EventType.DragPerform) {
+				DragAndDrop.AcceptDrag ();
 				Selection.activeGameObject = draggedObj;
 
-				if (draggedObj.GetComponent<PickUpPower> () || draggedObj.GetComponent<PickUpWeapon> ()) {
+				//Move the placed object into the correct location in the map hierarchy
+				if (draggedObj.GetComponent<PlatformController> ()) {
+					draggedObj.transform.SetParent (platforms);
+				} else if (draggedObj.GetComponent<PickUpPower> () || draggedObj.GetComponent<PickUpWeapon> ()) {
 					draggedObj.transform.SetParent (powerUps);
 				}
 
 				draggedObj = null;
 			}
 
-			Event.current.Use();
+			Event.current.Use ();
 		}
 
+		GameObject selected = Selection.activeGameObject;
+
+		if(selected) {
+			if (selected.GetComponent<PlatformController> () && platSnap) {
+				switch(Tools.current) {
+				case Tool.Move:
+					Vector3 mousePos = Event.current.mousePosition;
+					mousePos.y = sceneview.camera.pixelHeight - mousePos.y;
+					mousePos = sceneview.camera.ScreenToWorldPoint (mousePos);
+					mousePos.z = 0;
+					//Snap platform to grid
+					mousePos.x = Mathf.Round (mousePos.x / platSnapSize) * platSnapSize;
+					mousePos.y = Mathf.Round (mousePos.y / platSnapSize) * platSnapSize;
+					selected.transform.position = mousePos;
+					break;
+				case Tool.Scale:
+					Vector3 scale = selected.transform.localScale;
+					scale.x = Mathf.Round (scale.x / platSnapSize) * platSnapSize;
+					scale.y = Mathf.Round (scale.y / platSnapSize) * platSnapSize;
+					selected.transform.localScale = scale;
+					break;
+				}
+			}
+		}
 		Handles.EndGUI();
 	}
 
@@ -114,7 +155,7 @@ public class MapEditor : EditorWindow {
 		GUILayout.Label ("Map Settings", EditorStyles.boldLabel);
 		mapName = EditorGUILayout.TextField ("Map Name", mapName);
 
-		if (!mapInScene) {
+		if (!mapInScene || mapInfo == null) {
 
 			if (GUILayout.Button ("Generate Map \"" + mapName + "\"")) {
 				mapInScene = createMap ();
@@ -127,6 +168,30 @@ public class MapEditor : EditorWindow {
 
 			mousedOver = null;
 
+			EditorGUILayout.Separator ();
+			GUILayout.Label ("Platforms", EditorStyles.boldLabel);
+
+
+			foreach (GameObject go in placablePlatforms) {
+				Texture goTex;
+				if (goTex = go.GetComponent<MeshRenderer>().sharedMaterial.mainTexture) {
+					GUIContent content = new GUIContent (go.name + " Platform", goTex);
+					GUILayout.Box (content);
+
+					//Check if this box was moused over
+					if (GUILayoutUtility.GetLastRect ().Contains (Event.current.mousePosition)) {
+						mousedOver = go;
+					}
+				}
+			}
+
+			platSnap = EditorGUILayout.BeginToggleGroup ("Snap to Grid", platSnap);
+			platSnapSize = EditorGUILayout.FloatField ("Snap Grid Size", platSnapSize);
+			if (platSnapSize <= 0)
+				platSnapSize = 0.03125f;
+			EditorGUILayout.EndToggleGroup ();
+
+			EditorGUILayout.Separator ();
 			GUILayout.Label ("PowerUps", EditorStyles.boldLabel);
 
 			//EditorGUILayout.BeginHorizontal ();
@@ -144,6 +209,7 @@ public class MapEditor : EditorWindow {
 			}
 			//EditorGUILayout.EndHorizontal ();
 
+			EditorGUILayout.Separator ();
 			GUILayout.Label ("Weapons", EditorStyles.boldLabel);
 
 			//EditorGUILayout.BeginHorizontal ();
@@ -162,6 +228,7 @@ public class MapEditor : EditorWindow {
 			}
 			//EditorGUILayout.EndHorizontal ();
 
+			EditorGUILayout.Separator ();
 			if (meme = EditorGUILayout.Toggle ("Meme", meme)) {
 				SceneView.RepaintAll ();
 			}
