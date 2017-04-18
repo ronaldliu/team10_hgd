dec6bd8a4d11cdffe8a4f088146abe8f2da6f1f1
Jesse Moore <jessem@mtu.edu>
2017-04-17 21:29:05 -0400

    Fixin them bugs

---
+++ Assets/Scripts/Obstacles/FanWindScript.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FanWindScript : MonoBehaviour {
+
+	void OnTriggerStay2D(Collider2D other) {
+		transform.parent.GetComponent<FanScript> ().OnTriggerStay2D (other);
+	}
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
--- Assets/Scripts/SpiderController.cs
+++ Assets/Scripts/SpiderController.cs
@@ -3,6 +3,7 @@ using System.Collections;
 
 public class SpiderController : MonoBehaviour {
 	Rigidbody2D rBody;
+	Collider2D coll;
 	public int health = 100;
 	private Transform target;
 	private float direction;
@@ -10,43 +11,51 @@ public class SpiderController : MonoBehaviour {
 	private bool facingLeft = true;
 	private float distanceFromPlayer;
 	public float range = 20f;
-	private int foundPlayer = 0;
+	private bool foundPlayer = false;
 	public LayerMask canBeShot;
 	// Use this for initialization
 	void Start () {
 		if (GameObject.Find ("PlayerEnt")) {
 			target = GameObject.Find ("PlayerEnt").transform;
-			//foundPlayer = 1;
+			foundPlayer = true;
 		}
+		coll = transform.Find("body").GetComponent<Collider2D> ();
 		rBody = this.GetComponent<Rigidbody2D>();
 	}
 
 	// Update is called once per frame
 	void Update () {
-		if (foundPlayer == 0) {
-			if (GameObject.Find ("PlayerEnt")) {
-				target = GameObject.Find ("PlayerEnt").transform;
-				//foundPlayer = 1;
-			}
-		}
-		distanceFromPlayer = (transform.position.x - target.position.x);
-		if (distanceFromPlayer < 0)
-			distanceFromPlayer = distanceFromPlayer * -1;
+		if (GameObject.Find ("PlayerEnt")) {
+			target = GameObject.Find ("PlayerEnt").transform;
+			foundPlayer = true;
+			rBody.isKinematic = false;
+			coll.isTrigger = false;
 
-		//move enemy
-		if (distanceFromPlayer < range) {
-			if (!facingLeft && distanceFromPlayer > 1) {
-				transform.Translate (-Vector3.right * velocity * Time.deltaTime);
-			} else if(distanceFromPlayer > 1) {
-				transform.Translate (-Vector3.right * velocity * Time.deltaTime);
+
+			distanceFromPlayer = (transform.position.x - target.position.x);
+			if (distanceFromPlayer < 0)
+				distanceFromPlayer = distanceFromPlayer * -1;
+
+			//move enemy
+			if (distanceFromPlayer < range) {
+				if (!facingLeft && distanceFromPlayer > 1) {
+					transform.Translate (-Vector3.right * velocity * Time.deltaTime);
+				} else if(distanceFromPlayer > 1) {
+					transform.Translate (-Vector3.right * velocity * Time.deltaTime);
+				}
+
+				//should we change the sprites direction
+				direction = (transform.position.x - target.position.x);
+				if (direction > 0 && !facingLeft && distanceFromPlayer > 1)
+					Flip ();
+				else if (direction < 0 && facingLeft && distanceFromPlayer > 1)
+					Flip ();
 			}
 
-			//should we change the sprites direction
-			direction = (transform.position.x - target.position.x);
-			if (direction > 0 && !facingLeft && distanceFromPlayer > 1)
-				Flip ();
-			else if (direction < 0 && facingLeft && distanceFromPlayer > 1)
-				Flip ();
+		} else {
+			foundPlayer = false;
+			rBody.isKinematic = true;
+			coll.isTrigger = true;
 		}
 	}
 
@@ -59,19 +68,22 @@ public class SpiderController : MonoBehaviour {
 
 	public void applyDamage(int damage)
 	{
-		StartCoroutine (showDamaged ());
-		health -= damage;
-		if (health <= 0)
-			Destroy (gameObject);
+		if (foundPlayer) {
+			StartCoroutine (showDamaged ());
+			health -= damage;
+			if (health <= 0)
+				Destroy (gameObject);
+		}
 	}
 
 	IEnumerator showDamaged(){
-		SpriteRenderer sr = gameObject.GetComponent<SpriteRenderer> ();
-
-		sr.color = new Color (1f, 0f, 0f, .7f);
+		foreach (SpriteRenderer sr in gameObject.GetComponentsInChildren<SpriteRenderer>()) {
+			sr.color = new Color (0.7f, 0f, 0f, 1f);
+		}
 		yield return new WaitForSeconds (.1f);
-		sr.color = Color.white;
-	
+		foreach (SpriteRenderer sr in gameObject.GetComponentsInChildren<SpriteRenderer>()) {
+			sr.color = Color.white;
+		}
 	}
 	//bool playerInSight(){
 	//	RaycastHit2D hit = Physics2D.Raycast(Transform.position.x, target.position.x);
